57540dcc8fbb361d1282b4297f77b9cacf103661
Safayet N Ahmed <Safayet.Ahmed@ge.com>
2017-10-05 17:37:55 -0400

    Implemented the init utility.

--- utils/init.c
+++ utils/init.c
@@ -1,13 +1,34 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <uchar.h>
+#include "EFIGlue.h"
 
-static const char *usage = "";
+#include "EFIGlue.h"
+#include "BUMState.h"
+
+static const char *usage = "<BUM status directory> <starting config name>";
 
 int main(int argc, char** argv)
 {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "Usage: %s %s\n", argv[0], usage);
-    return -1;
+    int ret;
+    EFI_FILE_PROTOCOL *statusdir;
+    char *configname;
+    EFI_STATUS stat;
+
+    if(argc != 3){
+        fprintf(stderr, "Usage: %s %s\n", argv[0], usage);
+        ret = -1;
+    }else{
+        statusdir = GetDirFileProtocol(argv[1]);
+        configname = argv[2];
+        stat = BUMState_Init(statusdir, configname);
+        if(EFI_ERROR(stat)){
+            fprintf(stderr, "   BUMState_Init failed\n");
+            ret = -1;
+        }else
+            ret = 0;
+    }
+    return ret;
 }
 
