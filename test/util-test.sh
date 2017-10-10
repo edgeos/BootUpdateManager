8f90cc8e475a77ebcf4b7e7c4c5cda7dde762752
Safayet N Ahmed <Safayet.Ahmed@ge.com>
2017-10-10 11:26:27 -0400

    Added test cases for all boot possibilities. Added more descriptive prompts.

--- test/util-test.sh
+++ test/util-test.sh
@@ -1,5 +1,14 @@
 #/bin/bash
+set +x
+echo "*************************************************************************"
+echo "**** BUM utility tests"
+echo "*************************************************************************"
+echo
+echo "**** Argument-validation tests ****"
+echo
+
 set -x
+
 rm -rf test/statusdir
 
 bin/amd64/bumstate-init
@@ -14,13 +23,17 @@ bin/amd64/bumstate-print
 
 bin/amd64/bumstate-print test/statusdir
 
-bin/amd64/bumstate-update-start
-
-bin/amd64/bumstate-update-start test/statusdir/
-
 bin/amd64/bumstate-boottime-test
 
 bin/amd64/bumstate-boottime-test test/statusdir/
+
+bin/amd64/bumstate-runtime-init 
+
+bin/amd64/bumstate-runtime-init test/statusdir/
+
+bin/amd64/bumstate-update-start
+
+bin/amd64/bumstate-update-start test/statusdir/
 
 bin/amd64/bumstate-update-complete
 
@@ -34,36 +47,179 @@ bin/amd64/bumstate-update-complete test/statusdir/ 3 ""
 
 bin/amd64/bumstate-update-complete test/statusdir/ 3 sda3
 
+set +x
+
+echo "*************************************************************************"
+echo "**** Functionality tests ****"
+echo
+
+echo "*************************************************************************"
+echo "* Initialization and first boot tests"
+echo
+
+set -x
 bin/amd64/bumstate-init test/statusdir sda2
+set +x
 
 bin/amd64/bumstate-print test/statusdir
 
+set -x
 bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
 
 bin/amd64/bumstate-print test/statusdir
 
+set -x
+bin/amd64/bumstate-runtime-init test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+echo "*************************************************************************"
+echo "* Reboot after update start, before update completes"
+echo
+
+set -x
 bin/amd64/bumstate-update-start test/statusdir
+set +x
 
 bin/amd64/bumstate-print test/statusdir
 
+set -x
 bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
 
 bin/amd64/bumstate-print test/statusdir
 
+set -x
+bin/amd64/bumstate-runtime-init test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+echo "*************************************************************************"
+echo "* Failed complete update "
+echo
+
+set -x
+bin/amd64/bumstate-update-start test/statusdir
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+set -x
 bin/amd64/bumstate-update-complete test/statusdir/ 2 sda3
+set +x
 
 bin/amd64/bumstate-print test/statusdir
 
+#First reboot
+set -x
 bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
 
 bin/amd64/bumstate-print test/statusdir
 
+#Second reboot
+set -x
 bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
 
 bin/amd64/bumstate-print test/statusdir
 
+#Third reboot (Alternate)
+set -x
 bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
 
 bin/amd64/bumstate-print test/statusdir
 
+set -x
+bin/amd64/bumstate-runtime-init test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+echo "*************************************************************************"
+echo "* Successful complete update "
+echo
+
+set -x
+bin/amd64/bumstate-update-start test/statusdir
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+set -x
+bin/amd64/bumstate-update-complete test/statusdir/ 2 sda3
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+#First reboot
+set -x
+bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+set -x
+bin/amd64/bumstate-runtime-init test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+echo "*************************************************************************"
+echo "* Successful reboot without updates "
+echo
+
+set -x
+bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+set -x
+bin/amd64/bumstate-runtime-init test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+echo "*************************************************************************"
+echo "* Failed reboot without updates "
+echo
+
+#First reboot
+set -x
+bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+#Second reboot
+set -x
+bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+#Third reboot (Alternate)
+set -x
+bin/amd64/bumstate-boottime-test test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+set -x
+bin/amd64/bumstate-runtime-init test/statusdir/
+set +x
+
+bin/amd64/bumstate-print test/statusdir
+
+set -x
 rm -rf test/statusdir
+set +x
+
+echo "*************************************************************************"
+echo "**** End tests"
+echo "*************************************************************************"
