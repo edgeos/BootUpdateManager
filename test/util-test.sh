2f27ef3c4218f5333e855f1bea2de2d90eeeb06f
Safayet N Ahmed <Safayet.Ahmed@ge.com>
2017-10-10 07:57:30 -0400

    Current list of tests.

---
+++ test/util-test.sh
@@ -0,0 +1,45 @@
+#/bin/bash
+set -x
+rm -rf test/statusdir
+
+bin/amd64/bumstate-init
+
+bin/amd64/bumstate-init test/statusdir
+
+bin/amd64/bumstate-init test/statusdir sda2
+
+mkdir test/statusdir
+
+bin/amd64/bumstate-print
+
+bin/amd64/bumstate-print test/statusdir
+
+bin/amd64/bumstate-update-start
+
+bin/amd64/bumstate-update-start test/statusdir/
+
+bin/amd64/bumstate-update-complete
+
+bin/amd64/bumstate-update-complete test/statusdir/
+
+bin/amd64/bumstate-update-complete test/statusdir/ 3
+
+bin/amd64/bumstate-update-complete test/statusdir/ three sda3
+
+bin/amd64/bumstate-update-complete test/statusdir/ 3 ""
+
+bin/amd64/bumstate-update-complete test/statusdir/ 3 sda3
+
+bin/amd64/bumstate-init test/statusdir sda2
+
+bin/amd64/bumstate-print test/statusdir
+
+bin/amd64/bumstate-update-start test/statusdir
+
+bin/amd64/bumstate-print test/statusdir
+
+bin/amd64/bumstate-update-complete test/statusdir/ 3 sda3
+
+bin/amd64/bumstate-print test/statusdir
+
+rm -rf test/statusdir
