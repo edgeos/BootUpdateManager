5d7a004ae9b123d7bfa8db7fb4d38f9a6dea655f
Ryan Maloney <ryan.maloney@ge.com>
2018-01-12 08:42:44 -0500

    Updated grub.cfg with option to use either embedded hashlist or one offrom disk; the latter is for testing/demonstration purposes.

--- test/grub_make.sh
+++ test/grub_make.sh
@@ -6,6 +6,7 @@
 truncate -s 40k test/memdisk.tar
 
 tar -rf test/memdisk.tar grub.cfg
+tar -rf test/memdisk.tar hashlist.txt 
 
 # grub-mkimage builds payload.efi below:
 grub-mkimage -v -c grub_early.cfg -m test/memdisk.tar --prefix '' -O x86_64-efi -o test/payload.efi disk part_msdos part_gpt linux linux16 loopback normal configfile test search search_fs_uuid search_fs_file true search_label efi_uga efi_gop gfxterm gfxmenu gfxterm_menu fat ext2 ntfs cat echo ls help multiboot2 all_video efifwsetup gzio usbms password_pbkdf2 pbkdf2 verify gcry_rsa gcry_dsa gcry_sha512 gcry_sha1 crypto gcry_sha256 chain loadenv halt hashsum memdisk tar sleep lvm png multiboot
